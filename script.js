2d21c00f4ff192ff935be489ec39caf31e682245
Ginjak <89803801+Ginjak@users.noreply.github.com>
2025-02-20 19:55:22 +0000

    kdl section done

--- script.js
+++ script.js
@@ -41,5 +41,24 @@ document.addEventListener("DOMContentLoaded", function () {
         clickable: true,
       },
     });
+
+    const swiperKodel = new Swiper(".swiper-kodel", {
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+        renderFraction: function (currentClass, totalClass) {
+          return `<span class="${currentClass}"></span> / <span class="${totalClass}"></span>`;
+        },
+      },
+    });
   }
 });
